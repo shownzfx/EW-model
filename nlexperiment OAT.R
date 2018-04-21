5ff5d1632c993cd25f2e95865b04b4f16c7746fa
Fengxiu Zhang <shown05807@gmail.com>
2018-04-20 23:28:43 -0700

    comment windows

--- nlexperiment OAT.R
+++ nlexperiment OAT.R
@@ -1,11 +1,11 @@
 #EW model experiments
 
 # Windows
-library(nlexperiment)
-nl_netlogo_path("C:/Program Files/NetLogo 6.0.2/app")  #to netlogo installation on windows
-nl_netlogo_path()
-setwd("C:/Users/shown/Documents/GitHub/EW-model")
-module_file_path="C:/Users/shown/Documents/GitHub/EW-model/EW model using numeric chooser.nlogo"
+# library(nlexperiment)
+# nl_netlogo_path("C:/Program Files/NetLogo 6.0.2/app")  #to netlogo installation on windows
+# nl_netlogo_path()
+# setwd("C:/Users/shown/Documents/GitHub/EW-model")
+# module_file_path="C:/Users/shown/Documents/GitHub/EW-model/EW model using numeric chooser.nlogo"
 
 # AGAVE
 library(nlexperiment)
@@ -14,35 +14,9 @@ nl_netlogo_path()
 setwd("/home/fzhang59/dev/EW-model")
 module_file_path="/home/fzhang59/dev/EW-model/EW model using numeric chooser.nlogo"
 
+#---use one scenario at one time--------------
 
-chooseStrategy<- c("rememberFrequency","rememberCumDamage","rememberSevereDamage",
-                                    "riskPerception","doNothing")
-
-sample(chooseStrategy,nrow(paramsOAT),replace = T)
-
-paramsOAT<-  nl_param_oat(
-  n=25,
-  intensityThreshold = c(0,0.5,1),
-  orgBudget = c(0,4000,2000),
-  repairRatio=c(0,1,0.5),
-  extremeWeatherDamage=c(0,20,10),
-  adaptThreshold=c(0,10,5),
-  maxPrevention=c(0,20,10),
-  damageRatioThreshold=c(0,0.3,0.15),
-  interval=c(0,48,24),
-  numMonths = c(0,48,24),
-  riskPerceptionThreshold=c(0,150,75),
-  chooseStrategy= 1
-)
-
-
-
-paramsOAT<-paramsOAT %>% mutate(chooseStrategy=sample(chooseStrategy,nrow(paramsOAT),replace = T))
-
-
-
-
-experimentEW <- nl_experiment(
+experimentEW1 <- nl_experiment(
   model_file = module_file_path,
   iterations = 120,
   repetitions = 100,
@@ -90,6 +64,6 @@ experimentEW <- nl_experiment(
 )
 
 
-resultEW<-nl_run(experimentEW,parallel = T,print_progress = T)
-resultEW_backup <- resultEW
-write.csv(resultsEW,"nlexperiment results.csv")
+resultEW1<-nl_run(experimentEW1,parallel = T,print_progress = T)
+resultEW1_backup <- resultEW1
+write.csv(resultsEW1,"nlexperiment results.csv")
